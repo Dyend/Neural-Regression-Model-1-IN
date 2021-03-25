e6c76bc91f3df3fa35ca283d429674018887b787
Juan Pablo Raab Meza <jpraabm@hotmail.com>
2021-03-25 20:19:47 -0300

    :heavy_plus_sign: :hammer: Added Some lines and fixed some troubles.
    Added the output part of the project to make the train_y and test_y archives.
    Also fixed the functions random_reorder and normalizer so that they could work with a simple array instead of a matrix when the data  is a list.

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ def csv_to_matrix(ruta):
 
 def random_reorder(data):
     print('reordenando')
-    len_columna = len(data[0]) - 1
-    """ Swap las columnas de la matriz cantidad de columna * 2 veces"""
-    for i in range(1, len_columna * 2):
-        random_columna1 = randint(0, len_columna)
-        random_columna2 = randint(0, len_columna)
-        data[:,[random_columna1, random_columna2]] = data[:,[random_columna2, random_columna1]]
+    try:
+        len_columna = len(data[0]) - 1
+        """ Swap las columnas de la matriz cantidad de columna * 2 veces"""
+        for i in range(1, len_columna * 2):
+            random_columna1 = randint(0, len_columna)
+            random_columna2 = randint(0, len_columna)
+            data[:,[random_columna1, random_columna2]] = data[:,[random_columna2, random_columna1]]
+    except:
+        numpy.random.shuffle(data)
     return data
 
 def normalizer(data):
@@ -22,24 +25,29 @@ def normalizer(data):
     a = 0.01
     b = 0.99
     
-    """ iterando en las filas de la matriz"""
-    normalized_data = []
-    for fila in data:
-        max = numpy.amax(fila)
-        min = numpy.amin(fila)
-        fila_normalizada = list(map(lambda value: ((value - min) / (max - min)) * (b - a) + a, fila))
-        normalized_data.append(fila_normalizada)
-        #print(list(fila_normalizada))
-    normalized_data = numpy.array(normalized_data)
+    try:
+        """ iterando en las filas de la matriz"""
+        normalized_data = []
+        for fila in data:
+            max = numpy.amax(fila)
+            min = numpy.amin(fila)
+            fila_normalizada = list(map(lambda value: ((value - min) / (max - min)) * (b - a) + a, fila))
+            normalized_data.append(fila_normalizada)
+            #print(list(fila_normalizada))
+        normalized_data = numpy.array(normalized_data)
+    except:
+        max = numpy.amax(data)
+        min = numpy.amin(data)
+        normalized_data = list(map(lambda value: ((value - min) / (max - min)) * (b - a) + a, data))
+        normalized_data = numpy.array(normalized_data)
     return normalized_data
 
 if __name__ == "__main__":
     result_x = csv_to_matrix("./data/x_input.csv")
-    print(result_x)
-    #result_y = csv_to_matrix("./data/y_input.csv")
+    result_y = csv_to_matrix("./data/y_output.csv")
     result_x = random_reorder(result_x)
-    print(result_x)
+    result_y = random_reorder(result_y)
+    print(result_y)
     result_x = normalizer(result_x)
-    print(result_x)
-    print(len(result_x[0]))
-    #normalizer(random_reorder(result_y))
+    result_y = normalizer(result_y)
+    print(result_y)
