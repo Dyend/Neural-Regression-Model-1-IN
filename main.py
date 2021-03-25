1c5dc5f1ef16e8034d78f164d6b0ebf24d2028e2
Jose <alonso__96@hotmail.es>
2021-03-25 19:34:35 -0300

    added normalized function and random reorder

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 import csv
+import numpy
+from random import randint
 
-with open('./data/x_input.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+def csv_to_matrix(ruta):
+    file = open(ruta)
+    numpy_array = numpy.loadtxt(file, delimiter=",")
+    return numpy_array
 
+def random_reorder(data):
+    print('reordenando')
+    len_columna = len(data[0]) - 1
+    """ Swap las columnas de la matriz cantidad de columna * 2 veces"""
+    for i in range(1, len_columna * 2):
+        random_columna1 = randint(0, len_columna)
+        random_columna2 = randint(0, len_columna)
+        data[:,[random_columna1, random_columna2]] = data[:,[random_columna2, random_columna1]]
+    return data
 
-
-def normalizer():
-    max = data.max()
-    min = data.min()
+def normalizer(data):
+    
     a = 0.01
     b = 0.99
-    input_normalizado = map(lambda: ((value - min) / (max - min)) * (b - a) + a, data)
+    
+    """ iterando en las filas de la matriz"""
+    normalized_data = []
+    for fila in data:
+        max = numpy.amax(fila)
+        min = numpy.amin(fila)
+        fila_normalizada = list(map(lambda value: ((value - min) / (max - min)) * (b - a) + a, fila))
+        normalized_data.append(fila_normalizada)
+        #print(list(fila_normalizada))
+    normalized_data = numpy.array(normalized_data)
+    return normalized_data
 
 if __name__ == "__main__":
-    normalizer()
+    result_x = csv_to_matrix("./data/x_input.csv")
+    print(result_x)
+    #result_y = csv_to_matrix("./data/y_input.csv")
+    result_x = random_reorder(result_x)
+    print(result_x)
+    result_x = normalizer(result_x)
+    print(result_x)
+    print(len(result_x[0]))
+    #normalizer(random_reorder(result_y))
